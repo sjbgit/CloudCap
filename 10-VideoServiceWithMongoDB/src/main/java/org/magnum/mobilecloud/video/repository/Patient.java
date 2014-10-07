e042ef8d319a3006cbfb8d60b07f054aed960e50
sjbgit <sjb_strat@hotmail.com>
2014-10-07 17:54:33 -0500

    added model objects

---
+++ 10-VideoServiceWithMongoDB/src/main/java/org/magnum/mobilecloud/video/repository/Patient.java
@@ -0,0 +1,27 @@
+package org.magnum.mobilecloud.video.repository;
+
+import java.util.Collection;
+
+public class Patient extends User {
+
+	private long duration;
+	
+	public long getDuration() {
+		return duration;
+	}
+
+	public void setDuration(long duration) {
+		this.duration = duration;
+	}
+	
+	private Collection<Prescription> prescriptions;
+	
+	public Collection<Prescription> getPrescriptions() {
+		return prescriptions;
+	}
+
+	public void setPrescriptions(Collection<Prescription> prescriptions) {
+		this.prescriptions = prescriptions;
+	}
+		
+}
---
+++ 10-VideoServiceWithMongoDB/src/main/java/org/magnum/mobilecloud/video/repository/Prescription.java
@@ -0,0 +1,5 @@
+package org.magnum.mobilecloud.video.repository;
+
+public class Prescription {
+
+}
